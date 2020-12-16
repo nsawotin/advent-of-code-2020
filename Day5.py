83fa544cc960d0b45272e8a89a0e5956c8052016
Nadine Sawotin <nadines@pandell.com>
2020-12-15 20:24:58 -0700

    Day 5 part 1

---
+++ Day5.py
@@ -0,0 +1,47 @@
+path = "./Inputs/day5.txt"
+# path = "./Inputs/day5Test.txt"
+
+
+def part1():
+    seats = []
+
+    with open(path) as file:
+        for line in file.readlines():
+            rows = list(range(128))
+            cols = list(range(8))
+            letters = list(line.rstrip('\n'))
+
+            for letter in letters:
+                if(letter == 'F' or letter == 'B'):
+                    rows = splitList(rows, letter)
+                elif(letter == 'L' or letter == 'R'):
+                    cols = splitList(cols, letter)
+
+            row = rows[0]
+            col = cols[0]
+            seat = row * 8 + col
+            seats.append(seat)
+
+    print("Part 1:")
+    print(max(seats))
+
+
+def part2():
+
+    print("Part 2:")
+    print("TODO")
+
+
+def splitList(origList, part):
+    if (part == 'F' or part == 'L'):
+        # take first half
+        new_list = origList[:len(origList)//2]
+    else:
+        # take second half
+        new_list = origList[len(origList)//2:]
+
+    return new_list
+
+
+part1()
+part2()
